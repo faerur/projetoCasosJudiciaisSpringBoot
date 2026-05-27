620b1a52a7f3f7791402560116211f701f274199
Rafael Souza Santos <rafaelssantos201@gmail.com>
2026-05-27 08:17:10 -0300

    classe de seguranca web simples adicionada

---
+++ src/main/java/br/cnj/projeto/security/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package br.cnj.projeto.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	 auth.inMemoryAuthentication()
+	 .withUser("rafael").roles("USER").password("{noop}123")
+	 .and()
+	 .withUser("admin").roles("ADMIN").password("{noop}123");
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+
+	 return http.authorizeHttpRequests(request -> request.
+	requestMatchers(PathPatternRequestMatcher.pathPattern("/api/casos/**"))
+	 .hasRole("USER"))
+	 .authorizeHttpRequests(request -> request.requestMatchers(
+			 PathPatternRequestMatcher.pathPattern("/api/arquivos/**"))
+	 .hasRole("ADMIN")
+	 .anyRequest()
+	 .authenticated())
+	 .httpBasic(Customizer.withDefaults())
+	 .build();
+	}
+}
