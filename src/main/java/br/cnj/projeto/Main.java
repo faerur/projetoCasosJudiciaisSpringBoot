c257be01329a5fa21693879c2aa19682c70a999d
Rafael Souza Santos <rafaelssantos201@gmail.com>
2026-05-28 08:20:08 -0300

    comentando main adicional

---
+++ src/main/java/br/cnj/projeto/Main.java
@@ -0,0 +1,21 @@
+//package br.cnj.projeto;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//
+//@SpringBootApplication
+//public class Main implements CommandLineRunner{
+//	
+//	public static void main(String[] args) {
+//		SpringApplication.run(ProjetoApplication.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		
+////		System.out.println(caso.finalizarCusto(100, "RJ", 2024));
+//	}
+//
+//}
